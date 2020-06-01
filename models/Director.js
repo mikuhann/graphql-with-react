591df2b3ef5425b07ffd3073f4ee0056f209c371
Mikuhann <ng.kano@gmail.com>
2020-06-01 21:54:27 +0300

    add schemas

---
+++ models/Director.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema();
+
+const DirectorSchema = new Schema({
+  name: String,
+  age: Number,
+});
+
+module.exports = mongoose.model('Director', DirectorSchema);
---
+++ models/Movie.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema();
+
+const MovieSchema = new Schema({
+  name: String,
+  genre: String,
+  directorId: String,
+});
+
+module.exports = mongoose.model('Movie', MovieSchema);
